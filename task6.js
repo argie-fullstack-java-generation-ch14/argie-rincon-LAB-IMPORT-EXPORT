145a2e2f97cef8b2ff550d607791a25b94e388fb
argierincon <argierincon@gmail.com>
2026-09-25 14:19:12 -0500

    feat: task 6 completed

--- task6.js
+++ task6.js
@@ -1,3 +1,6 @@
-export function rubricaExcelente() {
+import { rubricaAprobadoReprobado } from './task5.js'
 
+export function rubricaExcelente(nota) {
+    if (nota > 8) return "Excelente";
+    return rubricaAprobadoReprobado(nota);
 }
