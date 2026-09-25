7dc306b8073989b09e7ebee9cd1c2dfe8a7fe3cf
argierincon <argierincon@gmail.com>
2026-09-25 14:11:54 -0500

    feat: task 4 completed

--- task4.js
+++ task4.js
@@ -1,3 +1,20 @@
+import { calculadoraEdad } from './task3.js'
+
 export class EdadAmigo {
+  constructor(name, year, month, day) {
+    this.name = name;
+    this.year = year;
+    this.month = month;
+    this.day = day;
+  }
+  
+  calcularEdad() {
+    return calculadoraEdad(this.year, this.month, this.day);
+  }
+
+  retornarEdad() {
+    const age = this.calcularEdad();
+    return `¡${this.name} tiene ${age} años hoy!`
+  }
 
 }
