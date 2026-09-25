a72c81970e17dcbbc2af174bcf658c61f24dbd47
argierincon <argierincon@gmail.com>
2026-09-25 14:03:45 -0500

    feat: task 3 completed

--- task3.js
+++ task3.js
@@ -1,3 +1,26 @@
-export function calculadoraEdad() {
+export function calculadoraEdad(y, m, d) {
 
+    const date = new Date();
+    const currentYear = date.getFullYear();
+    const currentMonth = date.getMonth() + 1;
+    const currentDay = date.getDate();
+
+    // console.log({ currentYear, currentMonth, currentDay });
+
+    let age = currentYear - y;
+
+    if (currentMonth < m) {
+        return age - 1;
+    }
+
+    if (currentMonth === m) {
+        if (currentDay < d) {
+            return age - 1;
+        }
+    }
+    // console.log({ age });
+
+    return age;
 }
+
+// calculadoraEdad();
